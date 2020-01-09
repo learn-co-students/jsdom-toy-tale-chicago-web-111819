e673f0327e5a1a3e78c75425f15b3a8bc897accf
github username <github email address>
2020-01-09 10:42:05 -0600

    fetched toys from json server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
-let addToy = false
+let addToy = false;
+const createH2 = document.createElement('h2')
+const createImg = document.createElement('img')
+const createP = document.createElement('p')
+const createBtn = document.createElement('button')
+const createDiv = document.createElement('div')
 
 document.addEventListener("DOMContentLoaded", ()=>{
-  const addBtn = document.querySelector('#new-toy-btn')
-  const toyForm = document.querySelector('.container')
+  const addBtn = document.querySelector('#new-toy-btn');
+  const toyForm = document.querySelector('.container');
+  const listToys = document.getElementById('toy-collection');
+
+  getToys();
+
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
-    addToy = !addToy
+    addToy = !addToy;
     if (addToy) {
-      toyForm.style.display = 'block'
+      toyForm.style.display = 'block';
     } else {
-      toyForm.style.display = 'none'
+      toyForm.style.display = 'none';
     }
   })
 
 })
+ 
+
+function getToys (){
+  fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+    .catch(error => console.error('oops, something is wrong'))
+}
+
+function displayToys(object){
+  for (const key in object){
+
+  }
+}
+
+// fetch("http://localhost:3000/users", configObj)
+//         .then(resp => resp.json())
+//         .then(json => document.body.innerHTML = json.id)
+//         .catch(error => document.body.innerHTML = 'Unauthorized Access')
+// }
