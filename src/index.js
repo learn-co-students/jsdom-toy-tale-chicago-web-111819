8b18c3658a9de0f4584883ab87f1ed4c7aff5c97
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-09 19:44:18 -0600

    features added, like button working/needs refactoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,98 @@
 let addToy = false
 
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+  .then(r => r.json() )
+  .then( renderToys )
+  .catch((err) => console.log('err', err))
+}
+
+function postToy(formData) {
+  const newToyObj = {
+    method: 'post',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData)
+  };
+
+  fetch('http://localhost:3000/toys', newToyObj)
+    .then(r => r.json() )
+    .then( buildToyElement )
+    .catch( console.log )
+}
+
+function renderToys(toys){
+  
+  toys.forEach( function(toy) {
+    buildToyElement(toy)
+  })
+}
+
+function buildToyElement(toy) {
+  const toyCollection = document.querySelector('#toy-collection')
+  const toyCard = document.createElement('div')
+  toyCard.className = 'card'
+
+  const toyName = document.createElement('h2')
+  toyName.innerText = toy.name
+
+  const toyIMG = document.createElement('img')
+  toyIMG.src = toy.image
+  toyIMG.width = 250
+  toyIMG.className = 'toy-avatar'
+
+  const toyLikes = document.createElement('p')
+  toyLikes.innerText = `${toy.likes} Likes`
+
+  const toyLiker = document.createElement('button')
+  toyCard.append(toyName)
+  toyCard.append(toyIMG)
+  toyCard.append(toyLiker)
+  toyCard.append(toyLikes)
+  toyLiker.id = toy.id
+  toyLiker.innerText = 'Like <3'
+  toyLiker.className = 'like-btn'
+  toyLiker.addEventListener('click', function(event) {
+    fetch(`http://localhost:3000/toys/${toyLiker.id}`, {
+      method: 'put',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "name": toyName.innerText,
+        "image": toyIMG.src,
+        "likes": toy.likes += 1
+      })
+    } )
+    .then( r => r.json() )
+    .then( data => toyLikes.innerText = `${data.likes} Likes`)
+    .catch(console.log)
+  })
+
+
+  toyCollection.append(toyCard)
+}
+
+
+
+function toyFormListener() {
+  const newToyForm = document.querySelector('form.add-toy-form')
+  newToyForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = {
+      "name": e.target[0].value,
+      "image": e.target[1].value,
+      "likes": 0
+    }
+    postToy(formData)
+  })
+}
+
+
+
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
+
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
@@ -12,5 +102,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
-
+  fetchToys();
+  toyFormListener();
+  
 })
+
+
+
